7be8d059b9b58d7e0bbd56fc57d67ce6e1d2ef1e
thomas_quenon@hotmail.fr <thomas.quenon@etudiant.univ-lille1.fr>
2015-04-03 16:28:57 +0200

    10 test

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -1,9 +1,22 @@
 package iut.tdd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Convert {
 	public static String num2text(String input) {
-		return null;
+		String[] num = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", 
+										"14", "15", "16"};
+		String[] text = new String[]{"zéro", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"};
+		Map<String, String> convert = new HashMap<String, String>();
+		
+		for(int i=0; i < 10; i++) {
+			convert.put(num[i], text[i]);
+		}
+		
+		return convert.get(input);
 	}
+	
 	public static String text2num(String input) {
 		return null;
 	}
